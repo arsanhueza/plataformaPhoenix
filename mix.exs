03b5829f8f7317d0dd0d8d895d08aa3a4e2185a9
Arturo Sanhueza <arturosanhueza@MacBook-Pro-de-Arturo.local>
2019-03-13 16:51:43 -0300

    cambio preuba

--- mix.exs
+++ mix.exs
@@ -54,9 +54,9 @@ defmodule Platform.Mixfile do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+      ecto.setup: ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      ecto.reset: ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
